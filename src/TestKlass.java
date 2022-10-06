54cc478e29fa328a4f71758cfd1d407aeaca794f
ViktorBack123 <viktor.back@rosendalsgymnasiet.se>
2022-10-06 13:59:59 +0200

    la till utskrift

--- src/TestKlass.java
+++ src/TestKlass.java
@@ -1,6 +1,7 @@
 public class TestKlass {
     public static void main(String[] args) {
         System.out.println("detta är viktor");
-        System.out.println("tja")
+        System.out.println("tja");
+        System.out.println("men nu är det bosse");
     }
 }
