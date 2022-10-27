2db87452fd7b5cea84b63357aec9434889ddc1b0
AxelMarhold1 <axel.marhold@rosendalsgymnasiet.se>
2022-10-27 16:31:00 +0200

    jeejj

--- src/Grafik.java
+++ src/Grafik.java
@@ -7,24 +7,24 @@ public class Grafik {
 
     JButton[] players = new JButton[15];
 
+    JTextField[] textFields = new JTextField[15];
+
+    JPanel panel = new JPanel();
+
+
     public Grafik() {
         // Fönstret:
         frame.setLayout(new FlowLayout());
         frame.setSize(frame.getMaximumSize());
         frame.getContentPane().setBackground(new Color(31, 183, 13, 180));
 
-
+        // Spelarna:
         for (int i = 0; i < 15; i++) {
             players[i] = new JButton();
             players[i].setPreferredSize(new Dimension(100, 100));
             players[i].setBorder(new LineBorder(Color.BLACK,8));
             frame.add(players[i]);
         }
-
-
-
-
-
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
