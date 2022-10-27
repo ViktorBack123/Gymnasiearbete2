e38d10f8aab222fe6c3d333ff2da95c4ad94ea24
AxelMarhold1 <axel.marhold@rosendalsgymnasiet.se>
2022-10-27 16:24:37 +0200

    jeejj

---
+++ src/Grafik.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+public class Grafik {
+    JFrame frame = new JFrame();
+
+    JButton[] players = new JButton[15];
+
+    public Grafik() {
+        // Fönstret:
+        frame.setLayout(new FlowLayout());
+        frame.setSize(frame.getMaximumSize());
+        frame.getContentPane().setBackground(new Color(31, 183, 13, 180));
+
+
+        for (int i = 0; i < 15; i++) {
+            players[i] = new JButton();
+            players[i].setPreferredSize(new Dimension(100, 100));
+            players[i].setBorder(new LineBorder(Color.BLACK,8));
+            frame.add(players[i]);
+        }
+
+
+
+
+
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+
+
+    public static void main(String[] args) {
+        new Grafik();
+    }
+}
--- src/TestKlass.java
+++ src/TestKlass.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class TestKlass {
+
+
+
+
     public static void main(String[] args) {
+
         System.out.println("detta är viktor");
         System.out.println("tja");
         System.out.println("men nu är det bosse");
