5924a9e43af5428aacc760fa0390754ff5859fc9
ViktorBack123 <viktor.back@rosendalsgymnasiet.se>
2023-03-07 14:08:13 +0100

    jeejj

---
+++ src/gugiuig.java
@@ -0,0 +1,8 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class gugiuig {
+    public static void main(String[] args) throws SQLException {
+        DriverManager.getConnection("");
+    }
+}
